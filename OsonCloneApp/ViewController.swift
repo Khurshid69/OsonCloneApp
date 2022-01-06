a079988989f7a5c147b83ab0d60d6ce6715320e3
khurshid <khurshidrakhimov06@gmail.com>
2022-01-06 19:55:47 +0500

    Initial Commit

---
+++ OsonCloneApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OsonCloneApp
+//
+//  Created by user on 06/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
